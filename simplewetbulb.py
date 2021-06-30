66f7e35c2a5e130c8739f0c3fd666bfe00b35adb
Dr-Kristau <dr.kristau@gmail.com>
2021-06-30 14:42:03 +0200

    text correction

--- simplewetbulb.py
+++ simplewetbulb.py
@@ -3,7 +3,7 @@ import numpy as np
 
 
 @click.command()
-@click.option('--temp', prompt='temperature in (ºC)', type=click.INT)
+@click.option('--temp', prompt='temperature (ºC)', type=click.INT)
 @click.option('--humid', prompt='relative humidity (%)', type=click.INT)
 def cli(temp, humid): 
     wetbulb = temp * np.arctan(0.151977 * np.power((humid + 8.313659), (1/2))) + np.arctan(temp + humid) - np.arctan(humid - 1.676331) + 0.00391838 * np.power(humid, (3/2)) * np.arctan(0.023101 * humid) - 4.686035
