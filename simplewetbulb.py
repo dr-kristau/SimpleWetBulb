3218ce61c476cee6da0c9c629d49a0b6cdc5e3f2
Dr-Kristau <dr.kristau@gmail.com>
2021-06-30 14:00:48 +0200

    consistent unit formats

--- simplewetbulb.py
+++ simplewetbulb.py
@@ -7,6 +7,6 @@ import numpy as np
 @click.option('--humid', prompt='relative humidity (%)', type=click.INT)
 def cli(temp, humid): 
     wetbulb = temp * np.arctan(0.151977 * np.power((humid + 8.313659), (1/2))) + np.arctan(temp + humid) - np.arctan(humid - 1.676331) + 0.00391838 * np.power(humid, (3/2)) * np.arctan(0.023101 * humid) - 4.686035
-    click.echo(f"wetbulb temperature ºC {np.format_float_positional(wetbulb, 2)}")
+    click.echo(f"wetbulb temperature (ºC): {np.format_float_positional(wetbulb, 2)}")
 
 
