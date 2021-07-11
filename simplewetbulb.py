4236353de6976c5fe8ab6ec679e5e15106dccaaf
Dr-Kristau <dr.kristau@gmail.com>
2021-07-11 17:40:42 +0200

    allow input as either ºF or ºC, print output as both

--- simplewetbulb.py
+++ simplewetbulb.py
@@ -2,33 +2,37 @@ import click
 import numpy as np
 
 @click.command()
-@click.option('--temp', prompt='temperature (ºC)', type=click.FLOAT)
+@click.option('--temp', prompt='temperature (ºC, ºF)', type=click.FLOAT)
 @click.option('--humid', prompt='relative humidity (%)', type=click.FLOAT)
-def cli(temp, humid): 
-    wetbulb = calc_wetbulb(temp, humid)
-    heatindex = calc_heatindex(temp, humid)
-    dewpoint = calc_dewpoint(temp, humid)
-    textwbt = f"wet-bulb temperature (ºC): {np.format_float_positional(wetbulb, 0, True, True, '-')}"
-    texthi = f"heat index (ºC): {np.format_float_positional(heatindex, 0, True, True, '-')}"
-    textdp = f"dew point (ºC): {np.format_float_positional(dewpoint, 0, True, True, '-')}"
+@click.option('--input', prompt='input temperature unit', type=click.Choice(['ºC','ºF'], case_sensitive=False), default='ºC', show_default='ºC')
+def cli(temp, humid, input): 
+    isCelc = (input == 'ºC')
+    tempC = temp if isCelc else fartoCelc(temp)
+    tempF = temp if not isCelc else celctoFar(temp)
+    wetbulbC = calc_wetbulb(tempC, humid)
+    heatindexF = calc_heatindex(tempF, humid)
+    dewpointC = calc_dewpoint(tempC, humid)
+    textwbt = f"wet-bulb temperature: {np.format_float_positional(wetbulbC, 1, True, True, '-')}ºC, {np.format_float_positional(celctoFar(wetbulbC), 1, True, True, '-')}ºF"
+    texthi = f"heat index: {np.format_float_positional(fartoCelc(heatindexF), 1, True, True, '-')}ºC, {np.format_float_positional(heatindexF, 1, True, True, '-')}ºF"
+    textdp = f"dew point: {np.format_float_positional(dewpointC, 1, True, True, '-')}ºC, {np.format_float_positional(celctoFar(dewpointC), 1, True, True, '-')}ºF"
     
-    if np.less_equal(wetbulb, fartoCelc(80)):
+    if np.less_equal(wetbulbC, fartoCelc(80)):
        click.echo(click.style(textwbt, fg='white'))
        click.echo(click.style(texthi, fg='white'))
        click.echo(click.style(textdp, fg='white'))
-    elif np.less_equal(wetbulb, fartoCelc(85)):
+    elif np.less_equal(wetbulbC, fartoCelc(85)):
        click.echo(click.style(textwbt, fg='green'))
        click.echo(click.style(texthi, fg='green'))
        click.echo(click.style(textdp, fg='green'))
-    elif np.less_equal(wetbulb, fartoCelc(88)):
+    elif np.less_equal(wetbulbC, fartoCelc(88)):
        click.echo(click.style(textwbt, fg='yellow'))
        click.echo(click.style(texthi, fg='yellow'))  
        click.echo(click.style(textdp, fg='yellow'))
-    elif np.less_equal(wetbulb, fartoCelc(90)):
+    elif np.less_equal(wetbulbC, fartoCelc(90)):
        click.echo(click.style(textwbt, fg='red')) 
        click.echo(click.style(texthi, fg='red'))  
        click.echo(click.style(textdp, fg='red'))  
-    elif np.greater(wetbulb, fartoCelc(90)):
+    elif np.greater(wetbulbC, fartoCelc(90)):
        click.echo(click.style(textwbt, bold=True, bg='white', fg='black')) 
        click.echo(click.style(texthi, bold=True, bg='white', fg='black')) 
        click.echo(click.style(textdp, bold=True, bg='white', fg='black'))    
@@ -39,11 +43,10 @@ def fartoCelc(tempF):
 def celctoFar(tempC):
     return (tempC * 1.8) + 32     
        
-def calc_wetbulb(temp, humid):
-    return temp * np.arctan(0.151977 * np.power((humid + 8.313659), (1/2))) + np.arctan(temp + humid) - np.arctan(humid - 1.676331) + 0.00391838 * np.power(humid, (3/2)) * np.arctan(0.023101 * humid) - 4.686035
+def calc_wetbulb(tempC, humid):
+    return tempC * np.arctan(0.151977 * np.power((humid + 8.313659), (1/2))) + np.arctan(tempC + humid) - np.arctan(humid - 1.676331) + 0.00391838 * np.power(humid, (3/2)) * np.arctan(0.023101 * humid) - 4.686035
         
-def calc_heatindex(temp, humid):
-    tempF = celctoFar(temp)
+def calc_heatindex(tempF, humid):
     hitemp = 61.0+((tempF-68.0)*1.2)+(humid*0.094)
     hifinal = 0.5*(tempF+hitemp)
     
@@ -62,10 +65,10 @@ def calc_heatindex(temp, humid):
             adj = adj1 * adj2
             hi = hi + adj
             
-    return fartoCelc(hi)
+    return hi
     
-def calc_dewpoint(temp, humid):
-    h = (np.log10(humid)-2)/0.4343 + (17.62*temp)/(243.12+temp) 
+def calc_dewpoint(tempC, humid):
+    h = (np.log10(humid)-2)/0.4343 + (17.62*tempC)/(243.12+tempC) 
     return 243.12*h/(17.62-h)   
 
 
